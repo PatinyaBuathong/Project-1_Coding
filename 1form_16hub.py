3c873bfef5ac90f031f95719d7d188151d02c8a4
PatinyaBuathong <patinyabuathong19@gmail.com>
2024-12-04 16:06:39 +0700

    Add files via upload

---
+++ 1form_16hub.py
@@ -0,0 +1,8 @@
+print("Welcome to Patinya Website")
+
+
+print("Hello I want to know your favourite ")
+singer = input("Who is your favourite singer?: ")
+food = input("What is your favourite food?: ")
+
+print("Wow it greats! I like", singer,"to and", food,"it is taste good!")
